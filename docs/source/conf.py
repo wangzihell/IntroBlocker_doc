2e0eee07f8bda50a65123b2e50cb3c12f9e748f4
wangzihell <wangzihell@gmail.com>
2021-11-09 15:51:44 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IntroBlocker'
+author = 'Zihao Wang'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
